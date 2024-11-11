e974943f4c65b2ce6b7d9f65d7eba8fa5f80cb8f
CK-EmbPro <kennydebrice2@gmail.com>
2024-11-11 21:57:27 +0200

    user auth

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import {contactRouter} from './routes/contactRoutes'
 import { subscriptionRouter } from "./routes/subscriptionRoutes";
 import { connectToDb } from "./config/dbConnection";
 import { blogRouter } from "./routes/blogRoutes";
+import { authRouter } from "./routes/authRoutes";
 // Load environment variables
 dotenv.config()
 
@@ -17,11 +18,12 @@ app.use(express.json())
 app.use('/api', contactRouter)
 app.use('/api',subscriptionRouter )
 app.use('/api', blogRouter)
+app.use('/api/auth', authRouter)
 app.get("/", (req: Request, res: Response)=>{
     res.send("Express with ts")
 })
 
-
+ 
 app.listen(port, ()=>{
     console.log(`running on http://localhost:${port}`);
 })
